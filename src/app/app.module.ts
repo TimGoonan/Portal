178b6330928e209685a0f1574f076e07a91f1bec
Joshua Archer <Joshua.Archer@greenwayhealth.com>
2017-07-10 10:40:42 -0400

    Login page with Home page getting setup with tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login'
+import { BillPage } from '../pages/bill/bill'
+import { MessagesPage } from '../pages/messages/messages'
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    BillPage,
+    MessagesPage
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage,
+    HomePage, 
+    BillPage,
+    MessagesPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+/*import { BillPage } from '../bill/bill'
+import { MessagesPage } from '../messages/messages'*/
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  rootPage:any = HomePage;
+  tab1: any;
+  tab2: any;
 
   constructor(public navCtrl: NavController) {
-
+/*      navCtrl.setRoot(HomePage);
+      this.tab1 = MessagesPage;
+      this.tab2 = BillPage;*/
   }
-
 }
