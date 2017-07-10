ca17f6c7044545d08d889f572af9b10db468f219
Joshua Archer <Joshua.Archer@greenwayhealth.com>
2017-07-10 10:42:39 -0400

    Login page with Home page getting setup with tabs

---
+++ src/pages/login/login.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home'
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+
+export class LoginPage {
+  username: string ;
+   pushPage: any;
+
+  constructor(public navCtrl: NavController) {
+    this.pushPage = HomePage;
+    this.username = '';
+  }
+
+}
